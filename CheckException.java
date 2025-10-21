61108b9dbcc5364adfbc056b119223753b8bb4b1
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-21 23:32:16 +0530

    finally usecase

---
+++ CheckException.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+//02:07:00
+
+public class CheckException {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        try{
+            //FileReader fr = new FileReader("text");
+            fis = new FileInputStream("test.txt");
+        }
+        catch(IOException e){
+            System.out.println("Error reading file...");
+            System.exit(0); //-> is there any way to stop finally? yes, stop JVM.
+        }
+        finally{
+            //fis.close();
+            System.out.println("finally");
+
+        }
+    }
+}

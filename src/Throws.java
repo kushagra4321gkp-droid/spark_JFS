39bd4f64ed183d9eda65c9c87db7802044eda47d
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-24 13:00:02 +0530

    Throws use case

--- src/Throws.java
+++ src/Throws.java
@@ -1,14 +1,16 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-public class Throws_VS_Throw {
+public class Throws {
     public static void main(String[] args) {
         //In java will use throws with method signature, when a method is declared with throws keyword, it means that the responsibility of handling exception is passed to the caller method.
         //in simple words, throws means -> Delegating your Responsibility to the caller method!
+        //we use throws keyword to deligate the responsibility of handling an exception to the caller method.
         try{
             m1();
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            //throw new RuntimeException(e);
+            System.out.println("exception");
         }
     }
 

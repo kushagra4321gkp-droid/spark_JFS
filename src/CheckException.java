443c5630874444ab42f4ae9c754016295ea8d849
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-24 10:46:25 +0530

    Moved Java files into src folder and updated code

--- src/CheckException.java
+++ src/CheckException.java
@@ -1,5 +1,4 @@
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 
 //02:07:00
--- src/Throws_VS_Throw.java
+++ src/Throws_VS_Throw.java
@@ -5,15 +5,20 @@ public class Throws_VS_Throw {
     public static void main(String[] args) {
         //In java will use throws with method signature, when a method is declared with throws keyword, it means that the responsibility of handling exception is passed to the caller method.
         //in simple words, throws means -> Delegating your Responsibility to the caller method!
+        try{
+            m1();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void m1(){
+    public static void m1() throws FileNotFoundException{
         m2("test.txt");
         System.out.println("m1");
     }
 
     public static void m2(String fileName) throws FileNotFoundException{
 
-        FileReader file = new FileReader("fileName");
+        FileReader file = new FileReader(fileName);
     }
 }

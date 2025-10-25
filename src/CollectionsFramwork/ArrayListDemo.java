94034a96f71f702e6b78a00d9474e00b0edfa635
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-25 20:04:16 +0530

    ArrayList creation and some methods

---
+++ src/CollectionsFramwork/ArrayListDemo.java
@@ -0,0 +1,41 @@
+package CollectionsFramwork;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        //Order preserved
+        //Duplicate Allowed
+        //Index wise access
+
+        //Can Parent hold Child's Object? see
+        List<Integer> nums = new ArrayList<>();
+        nums.add(10);
+        nums.add(90);
+        nums.add(30);
+        nums.add(10);
+        nums.add(90);
+        System.out.println(nums);
+        System.out.println(nums.get(0));
+
+        //Some Common Methods are :-
+        //size()
+        //add()
+        //remove()
+        //clear()
+        //add(idx, ele)
+        //isEmpty()
+
+        System.out.println(nums.size());
+
+        if(nums.isEmpty()){
+            System.out.println("empty...");
+        }else{
+            System.out.println("not empty...");
+        }
+
+        nums.add(0, 50);
+        System.out.println(nums);
+    }
+}

33a35c70299fbc4d487e3347f2b28e9686ad953c
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-27 14:11:19 +0530

    Generics Depth

--- src/CollectionsFramwork/Generics_/WithoutGenerics.java
+++ src/CollectionsFramwork/Generics_/WithoutGenerics.java
@@ -1,4 +1,28 @@
 package CollectionsFramwork.Generics_;
 
+import java.util.ArrayList;
+
 public class WithoutGenerics {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(2);
+        al.add(3);
+        System.out.println(al);
+
+        al.add("kushagra");
+        al.add("tiwar");
+        System.out.println(al);
+
+        //No type safety. that's why Generics are used.
+
+//        ArrayList<Integer> al2 = new ArrayList<>();
+//        al2.add(10);
+//        al2.add("kushagra"); --> it will not work because we have specified the Generics!
+
+
+        ArrayList al3 = new ArrayList(Integer.parseInt("Kushagra"));
+
+
+
+    }
 }
--- src/CollectionsFramwork/Generics_/withGenerics.java
+++ src/CollectionsFramwork/Generics_/withGenerics.java
@@ -1,4 +1,29 @@
 package CollectionsFramwork.Generics_;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class withGenerics {
+    public static void main(String[] args) {
+        List<Integer> al = new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        System.out.println(al);
+
+        //how Java polymorphism + generics + interfaces connect together.
+//        al = new Stack<>();
+//        al.push(10);
+//        al.push(20);
+//        System.out.println(al);
+
+        //It works
+        al = new Stack<>(); //either if you want to use push() so you have to type cast it into stack
+        al.add(10);
+        al.add(20);
+        System.out.println(al);
+
+
+
+    }
 }

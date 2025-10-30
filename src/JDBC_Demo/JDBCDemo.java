503f33bda308143cd7f9b5fa6b287c9c319861f7
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-10-30 23:06:22 +0530

    Configuring mysql workbench

---
+++ src/JDBC_Demo/JDBCDemo.java
@@ -0,0 +1,18 @@
+package JDBC_Demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCDemo {
+    //(1)load the Driver class
+    //(2)Get Connection form db
+    //(3)Create Statement
+    //(4)Execute Query
+
+    public static void main(String[] args) {
+        Class.forName("com.mysql.cj.jdbc.Driver"); //load Driver class
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mystd", "root", "root");
+
+
+    }
+}

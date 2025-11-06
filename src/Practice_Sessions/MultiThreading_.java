4babd6a2d931b35f9568b9c36a086826c7d95b7a
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-11-06 20:08:27 +0530

    multiThreading practice

---
+++ src/Practice_Sessions/MultiThreading_.java
@@ -0,0 +1,40 @@
+package Practice_Sessions;
+
+class myThread1 extends Thread{
+    @Override
+    public void run() {
+        for(int i = 1; i<=5; i++){
+            System.out.println("t1");
+        }
+    }
+}
+
+class myThread2 extends Thread{
+    @Override
+    public void run() {
+        for(int i = 1; i<=5; i++){
+            System.out.println("t2");
+            try {
+                myThread2.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+public class MultiThreading_ {
+    public static void main(String[] args) throws InterruptedException {
+        //System.out.println(Thread.currentThread().getName());
+        myThread1 t1 = new myThread1();
+        myThread2 t2 = new myThread2();
+        t1.start();
+        t1.join();
+        t2.start();
+
+            for(int i = 1; i<=5; i++){
+                System.out.println("main");
+            }
+
+    }
+}

88903ba4af8b1054bbce4b3dbd382b80af744418
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-11-08 15:30:09 +0530

    Servlet _ Tomcat _ maven etc

--- src/Servlet/Servlet_Demo.java
+++ src/Servlet/Servlet_Demo.java
@@ -1,4 +1,16 @@
 package Servlet;
 
 public class Servlet_Demo {
+    //Flow:
+    //1.Browser hit : http://abcd.in/login
+    //2.DNS lookup
+    //    a. Resolves http://abcd.in/login -> 10.22.123.44
+    //3.TCP handshake and establishing connection
+    //4.HTTP request sent via TCP stream
+    //   a.TCP breaks HTTP into packets
+    //   b.Each packet has sequence number
+    //   c.IP route them to server
+    //   d.Server read that request and return response
+    //5.HTTP response sent via TCP
+    //6.TCP close the connection
 }

80c1b2734f6e46733b03fdf2c19cc12a9a5ffef3
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-11-11 14:34:42 +0530

    Spring Modules

--- src/SpringFrameWork/SpringCore.java
+++ src/SpringFrameWork/SpringCore.java
@@ -1,5 +1,16 @@
 package SpringFrameWork;
 
 public class SpringCore {
-
+    //Spring core
+    //Spring context
+    //Spring JDBC
+    //Spring ORM
+    //Spring AOP
+    //Spring Web MVC
+    //Spring Security
+    //Spring Batch
+    //Spring Data JPA
+    //Spring Rest
+    //Spring Cloud
+    //Spring AI
 }

3216b5437534514168efdfbd63e71c7f91958293
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2025-11-13 10:02:24 +0530

    Constructor Injection

--- src/DependencyInjectionDemo/App.java
+++ src/DependencyInjectionDemo/App.java
@@ -2,8 +2,13 @@ package DependencyInjectionDemo;
 
 public class App {
     public static void main(String[] args) {
-        Student s = new Student();
-        Course course = new DSA();
-        s.study(course);
+        //Dependency Injection using Setter() method
+//        Student s = new Student();
+//        Course course = new DSA();
+//        s.study(course);
+
+        //Dependency Injection using Constructor()
+        Student s = new Student(new DSA());
+        s.study();
     }
 }
--- src/DependencyInjectionDemo/Student.java
+++ src/DependencyInjectionDemo/Student.java
@@ -3,11 +3,26 @@ package DependencyInjectionDemo;
 public class Student {
     private Course course;
 
+    public Student() {
+    }
+
+    public Student(Course course) {
+        this.course = course;
+    }
+
     public void setCourse(Course course) {
         this.course = course;
     }
 
-    public void study(Course course){
+//    public void study(Course course){
+//        if(course.enroll()>=1){
+//            System.out.println("Journey Started...");
+//        }else {
+//            System.out.println("Payment failed...");
+//        }
+//    }
+
+    public void study(){
         if(course.enroll()>=1){
             System.out.println("Journey Started...");
         }else {
